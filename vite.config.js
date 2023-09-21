ab9649fa5e67e6932dface2f6365951352b9203b
anthonyreynaldi <77012214+anthonyreynaldi@users.noreply.github.com>
2023-09-21 19:57:31 +0700

    change vite config to add subfolder name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/rw5-green-house/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['react-qr-code']
